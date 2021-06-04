d46d0b8e8c144fb64599c53641931111caa58fae
estevandh <estevandelabarihenriques@gmail.com>
2021-06-04 11:52:53 -0300

    121/@Eduardo-LP @estevandh:Cria o pacote util para os services do jwt

--- backend/src/main/java/com/thoughtworks/aceleradora/controller/LoginController.java
+++ backend/src/main/java/com/thoughtworks/aceleradora/controller/LoginController.java
@@ -1,9 +1,13 @@
 package com.thoughtworks.aceleradora.controller;
 
+import com.thoughtworks.aceleradora.service.util.JwtUtil;
 import com.thoughtworks.aceleradora.controller.response.LoginResponse;
 import com.thoughtworks.aceleradora.service.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import javax.ws.rs.POST;
 
@@ -11,6 +15,10 @@ import javax.ws.rs.POST;
 @RequestMapping("/login")
 
 public class LoginController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping
     public String returnAPI(@RequestBody User user) {
 
@@ -19,6 +27,19 @@ public class LoginController {
         String data = login.returnLogin(user.getUser(), user.getPassword());
         return data;
 
+    }
+
+    @PostMapping("/testeJWT")
+    public String generateToken(@RequestBody User user) throws Exception {
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUser(), user.getPassword())
+            );
+        } catch (Exception ex) {
+            throw new Exception("inavalid username/password");
+        }
+        return JwtUtil.generateToken(user.getUser());
     }
 
 }
