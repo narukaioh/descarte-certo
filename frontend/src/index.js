347f5cea30a1250b18ef910ccee3f1a394566918
Ju Dantas <jucienyds@gmail.com>
2021-06-18 22:21:17 -0300

    remove links

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,9 +13,6 @@ ReactDOM.render(
   <HashRouter>
     <Switch>
       <Route exact path="/" component={App} />
-      <Route exact path="/contato" component={Footer} />
-      <Route exact path="/beneficios" component={Benefits} />
-      <Route exact path="/orcamentos" component={OrderListComponent} />
       <Route exact path="/login" component={LoginPage} />
     </Switch>
 
